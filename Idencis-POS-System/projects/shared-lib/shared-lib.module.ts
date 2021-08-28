e63a509268fd0b2d412d69dbecd855319b240e51
keyurkhant <kpkhant007@yahoo.com>
2021-08-28 23:13:57 +0530

    First Frontend Structure

---
+++ Idencis-POS-System/projects/shared-lib/shared-lib.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedLibRoutingModule } from './shared-lib-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SharedLibRoutingModule
+  ]
+})
+export class SharedLibModule { }
