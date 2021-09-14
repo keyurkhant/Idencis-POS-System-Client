c03cc5de7cec77e5615f8009a8228f3803d33c50
keyurkhant <kpkhant007@yahoo.com>
2021-09-14 15:47:48 +0530

    added sidebar,forgotPassword,login UI

--- Idencis-POS-System/projects/shared-lib/shared-lib.module.ts
+++ Idencis-POS-System/projects/shared-lib/shared-lib.module.ts
@@ -3,19 +3,27 @@ import { CommonModule } from '@angular/common';
 
 import { SharedLibRoutingModule } from './shared-lib-routing.module';
 import { LoginComponent } from './components/login/login.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    ForgotPasswordComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule,
-    SharedLibRoutingModule
+    SharedLibRoutingModule,
+    NgbModule
   ],
   exports: [
     CommonModule,
-    LoginComponent
+    LoginComponent,
+    ForgotPasswordComponent,
+    SidebarComponent
   ]
 })
 export class SharedLibModule { }
